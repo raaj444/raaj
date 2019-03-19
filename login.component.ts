722312da95f407993947d486f47a39af04fb08b3
raaj444 <48557300+raaj444@users.noreply.github.com>
2019-03-20 05:04:20 +0530

    Add files via upload

---
+++ login.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username='';
+  password='';
+  errorMessages='Invalid Credentials';
+  invalidLogin= false;
+
+  constructor(private router: Router,private authService: HardcodedAuthenticationService) { }
+
+  handleLogin()
+  {
+    if(this.authService.authenticate(this.username,this.password))
+    {
+     this.router.navigate(['welcome',this.username])
+      this.invalidLogin=false
+    }
+    else{
+     
+      this.invalidLogin=true
+    }
+  }
+  ngOnInit() {
+  }
+
+}
+
